97ea5667950a3d8f72293b01d88e786905c216f6
alberto-demiguel <ademiguellechuga@gmail.com>
2025-01-04 20:52:35 +0100

    Comments generate_schema_name.sql

--- dbt/macros/generate_schema_name.sql
+++ dbt/macros/generate_schema_name.sql
@@ -1,3 +1,7 @@
+-- This macro is intended to override the "target_schema" specified in the profiles.yml file
+-- If the target is "pro", every model will be materialized in the "<directory_name>" schema within the "pro" database. For instance: a model located in the "dbt/models/silver" directory will be materialized in the schema "pro.silver"
+-- If the target is "dev", every model will be materialized in the "<user>_<directory_name>" shcema within the "dev" database. For instance: a model located in the "dbt/models/silver" directory will be materialized in the schema "dev.<user>_silver"
+
 {% macro generate_schema_name(custom_schema_name, node) %}
   {% set user = target.user %}
   {% set environment = target.name %}
