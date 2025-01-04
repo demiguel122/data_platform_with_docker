d786443588a0ef9c0e353ce00246894178a65f6b
alberto-demiguel <ademiguellechuga@gmail.com>
2025-01-04 19:53:12 +0100

    Comments DAG

--- airflow/dags/forex__customer_transactions__hourly.py
+++ airflow/dags/forex__customer_transactions__hourly.py
@@ -1,6 +1,4 @@
 from airflow.decorators import dag, task
-from airflow.models import Variable
-from airflow.models import Connection
 from pendulum import datetime, duration
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.ssh.operators.ssh import SSHOperator
@@ -8,13 +6,16 @@ from airflow.hooks.base_hook import BaseHook
 import os
 import pandas as pd
 
+# Note the absence of top-level DAG code
+# This is done to avoid the Airflow scheduler from executing this code at least every 30s rather than when the DAG is scheduled to run, causing unnecessary stress on the infrastructure
+
 @dag(
-    dag_id="FOREX__CUSTOMER_TRANSACTIONS__HOURLY",
-    description="This DAG implements a full, scalable and memory-efficient pipeline orchestration setup to load .csv data into a PostgreSQL database.",
-    tags=["FINANCE", "FX_TRANSFERS"],
-    owner_links={"admin": "mailto:admin@example.com"},
-    start_date=datetime(2023, 12, 20),
-    schedule_interval=None,
+    dag_id="FOREX__CUSTOMER_TRANSACTIONS__HOURLY",      # DAG nomenclature: <company_division>__<pipeline>__<schedule_interval>
+    description="This DAG implements a full, scalable and memory-efficient ELT pipeline orchestration setup to load .csv data containing customer transaction data into a PostgreSQL database and transform it using dbt.",
+    tags=["forex", "transactions", "hourly"],
+    owner_links={"data_platform_engineer": "mailto:data_platform_engineer@fintech_company.com"},
+    start_date=datetime(2025, 1, 1),
+    schedule_interval=None,     # Set to none for the sake of convenience. Simply trigger manually when deploying
     catchup=False,
     default_args={
         "retries": 3,
@@ -24,6 +25,7 @@ import pandas as pd
 )
 def orchestrator():
         
+    # Splits the .csv file in chunks, saves each chunk as a separate file in a local temporary directory created ad-hoc and returns the path to each chunk
     @task
     def extract(file_path: str, chunk_size: int) -> list:
         file_name_with_extension = os.path.basename(file_path)
@@ -38,6 +40,9 @@ def orchestrator():
             chunk_paths.append(chunk_path)
         return chunk_paths
 
+    # Dynamically generates a MERGE statement for each chunk to perform upsert operations into the target Postgres database
+    # This task is intended to leverage Airflow's Dynamic Task Mapping to load the csv chunks in parallel (see the use of the "expand" method below)
+    # Note that it also instructs Postgres to generate a "load_timestamp" value for each inserted/updated record. This will be used by incremental models in dbt.
     @task      
     def load(chunk_path: str, target_table: str, primary_key: str):
         postgres_hook = PostgresHook(postgres_conn_id='postgres_dbt_conn')
@@ -49,7 +54,7 @@ def orchestrator():
         table_columns = csv_columns
 
         values_clause = ", ".join(
-        f"({', '.join([repr(row[col]) if not pd.isna(row[col]) else 'NULL' for col in csv_columns])})"
+        f"({', '.join([repr(row[col]) if not pd.isna(row[col]) else 'NULL' for col in csv_columns])})"  # Converts missing values to SQL NULL's
         for _, row in df.iterrows()
         )
 
@@ -72,8 +77,14 @@ def orchestrator():
 
         os.remove(chunk_path)  # Clean up the temporary file after loading
     
+    # Retrieves "extra" connection details as a JSON object
+    # This is necessary because, eventhough the necessary environment variables are set in the dbt container with docker-compose, the SSHOperator opens a non-interactive shell
+    # This results in remote environment variables not being sourced, so they must be passed using the "environment" parameter below
     dbt_conn_extra = BaseHook.get_connection('dbt_conn').extra_dejson
 
+    # Triggers the dbt transformation process targeting the production environment defined in the profiles.yml file
+    # Prior to that, installs all necessary dependencies included in the packages.yml file
+    # IMPORTANT: if you are using the "Power User for dbt" VS Code extension, you must disable it before executing "docker-compose build", since it misinteracts with the "dbt deps" command, causing it to fail
     transform = SSHOperator(
         task_id='transform',
         ssh_conn_id='dbt_conn',
@@ -86,9 +97,10 @@ def orchestrator():
         cmd_timeout=600
     )
 
-    # Orchestration
+    # Task dependencies
     chunk_files = extract(file_path='/opt/airflow/data/customer_transactions.csv', chunk_size=10)
     load_task = load.expand(chunk_path=chunk_files, target_table=['forex.customer_transactions'], primary_key =['transaction_id'])
     load_task >> transform
 
-orchestrator() # Instantiate the dag
+# Instantiates the dag
+orchestrator()
